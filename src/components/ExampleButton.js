db6a02bc19b2def2d867f430ab564cf0b97986d6
Amr <asym17182@gmail.com>
2026-01-31 23:24:54 +0400

    added initial component folder without any clean up (ill do it tmrw)

---
+++ src/components/ExampleButton.js
@@ -0,0 +1,11 @@
+import React from 'react' /* Included for using states */
+
+export default function ExampleButton() {
+  /* all main logic of the component here */
+
+
+  /* the actual jsx component here */
+  return (
+    <button> i am useless </button>
+  )
+}
