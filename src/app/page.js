d1b6475f49148f20b7214b2a6738359f4c0b6214
Amr <asym17182@gmail.com>
2026-02-01 15:47:41 +0400

    template header

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+/* GET STARTED PAGE */
 import ExampleButton from "@/components/ExampleButton.js"
+import UserHeader from "@/components/UserHeader.js"
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
+      <UserHeader />
       <main>
         <ExampleButton />
         <Link href="/auth/signup"> Signup's </Link>
---
+++ src/components/UserHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function UserHeader() {
+
+  return (
+    <header>
+
+      <Image src="" width="200" height="200" />
+      <a> PulseLink  </a>
+    </header>
+  );
+}
