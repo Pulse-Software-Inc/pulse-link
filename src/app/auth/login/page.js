2f31745f2ac148079e278701b071edb474ce2192
Amr <asym17182@gmail.com>
2026-04-03 21:51:25 +0400

    login authentication

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -7,6 +7,10 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
+{/* Firebase JS SDK For Authentication */ }
+import { auth } from '@/lib/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
 export default function LoginPage() {
   const router = useRouter();
   const [loginData, setLoginData] = useState({
@@ -15,12 +19,27 @@ export default function LoginPage() {
     acceptTerms: false,
   });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Handle login logic here
     console.log('Login attempt:', { loginData });
     // Only redirects when login is successful
-    router.push('/main/prof-dashboard');
+    const userCredential = await signInWithEmailAndPassword(auth, loginData.email, loginData.password); // Mention Firebase Security Features, Fails if Email exists
+    const idToken = await userCredential.user.getIdToken();
+    const response = await fetch('http://localhost:8000/api/v1/users/me', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${idToken}`,
+        'Content-Type': 'application/json',
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    if (response.ok) {
+      router.push('/auth/login');
+    }
   };
 
   return (
@@ -60,6 +79,8 @@ export default function LoginPage() {
                 type="email"
                 id="email"
                 placeholder="john@gmail.com"
+                value={loginData.email}
+                onChange={(e) => setLoginData({ ...loginData, email: e.target.value })}
                 className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent"
                 required
               />
@@ -74,6 +95,8 @@ export default function LoginPage() {
                 type="password"
                 id="password"
                 placeholder="••••••••••"
+                value={loginData.password}
+                onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
                 className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent"
                 required
               />
