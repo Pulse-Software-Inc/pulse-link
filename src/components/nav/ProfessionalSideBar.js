1ce0c112b776b1d3c0a94ea8733d2c02501c418a
fuzzyeh <fuzzyahmed25@gmail.com>
2026-04-04 17:10:11 +0400

    fixed the paths to the images

--- src/components/nav/ProfessionalSideBar.js
+++ src/components/nav/ProfessionalSideBar.js
@@ -70,7 +70,7 @@ function DoctorSidebar() {
 
                 {/* Sign Out Button */}
                 <Link href="/auth/signout" className="w-full flex items-center gap-3 px-4 py-3 rounded-lg text-red-500 font-semibold hover:bg-white/20 transition-all">
-                    <Image src="/LogOut_Icon.svg" alt="Sign out" width={24} height={24} />
+                    <Image src="/Sidebar/LogOut_Icon.svg" alt="Sign out" width={24} height={24} />
                     <span>Sign out</span>
                 </Link>
             </nav>
