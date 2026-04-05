890a4a57e6091e395e887d51d90bda6da8c38be9
Amr <asym17182@gmail.com>
2026-04-05 16:58:36 +0400

    add boilerplate and updated sidebar variables to be passed as props

--- src/components/util/Settings.js
+++ src/components/util/Settings.js
@@ -9,34 +9,35 @@ import {
   FormControlLabel,
 } from "@mui/material"
 import Image from "next/image"
-import { SettingsSidebar } from "@/components/util/SettingsSidebar"
+import SettingsSidebar from "@/components/util/SettingsSidebar"
+import UserProfileSection    from "./sections/UserProfileSection"
+import AuthorizedAccessSection from "./sections/AuthorizedAccessSection"
+import DataPrivacySection    from "./sections/DataPrivacySection"
+import AppearanceSection     from "./sections/AppearanceSection"
+import GoalsSection          from "./sections/GoalsSection"
+import NotificationsSection  from "./sections/NotificationsSection"
+import InviteClientsSection  from "./sections/InviteClientsSection"
+
 
 export default function Settings(props) {
   // GET current settings from backend
   const currentSettings = {}
 
   // Initial Configurations
-  let sectionIDs = [
-    "user-profile",
-    "data-privacy",
-    "appearance",
-    "notifications"
+  let sidebarLabels = [
+    { id: "user-profile", label: "User Profile" },
+    { id: "authorized-access", label: "Authorized Access" },
+    { id: "data-privacy", label: "Data and Privacy" },
+    { id: "appearance", label: "Appearance" }
   ]
   if (currentSettings.role == 'user')
-    sectionIDs.push("goals", "custom-instructions")
+    sectionIDs.push({ id: "goals", label: "Goals" },
+      { id: "notifications", label: "Notifications" })
   else
-    sectionIDs.push("invite-clients")
-
-  const [editing, setEditing] = useState(false)
-  const [firstName, setFirstName] = useState("First Name")
-  const [lastName, setLastName] = useState("Last Name")
-  const [email, setEmail] = useState("**********@email.com")
-  const [phone, setPhone] = useState("*********1234")
-  const [emailError, setEmailError] = useState("")
-  const [phoneError, setPhoneError] = useState("")
+    sectionIDs.push({ id: "invite-clients", label: "Invite Clients" })
 
   // Handlers
-  pass;
+
 
   return (
     <>
